1fc53ec9880b47ae39ec955fbfbbfebc21beebc0
Collinsoden22 <Collinsoden22@gmail.com>
2023-12-07 17:05:02 +0100

    Added overlay to modal

--- js/main.js
+++ js/main.js
@@ -2,17 +2,17 @@
 function isLoggedIn() {
     // Replace this with your actual authentication check
     // For example, you might use cookies, session storage, or an authentication API
-    return false; // Return true if the user is logged in, false otherwise
+    return true; // Return true if the user is logged in, false otherwise
 }
 
 // Function to open the modal
 function openModal() {
     if (isLoggedIn()) {
         document.getElementById("myModal").style.display = "block";
-        document.getElementById("overlay").style.display = "block";
     } else {
         document.getElementById("loginMessage").style.display = "block";
     }
+    document.getElementById("overlay").style.display = "block";
 }
 
 // Function to close the modal
